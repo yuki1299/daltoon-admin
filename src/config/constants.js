04cdd40b2f739fa9713f6510832b4f48d072479b
Eduardo Zaghi <eduardo@loldesign.com.br>
2018-01-08 17:56:46 -0200

    setting staging url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,7 @@
 const constants = {
   api: {
-    path: 'http://localhost:8000'
+    path: 'http://staging-api.matchmap.loldesign.com.br'
+    // path: 'http://localhost:8000'
   }
 }
 export default constants;
