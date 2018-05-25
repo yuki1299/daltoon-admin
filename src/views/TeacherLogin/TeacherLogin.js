ee265e85074349c5ca3bef0d3d53c01b99289cda
Vinicius Lima <vinicius@loldesign.com.br>
2018-05-25 01:09:23 -0300

    First Commit

--- src/views/TeacherLogin/TeacherLogin.js
+++ src/views/TeacherLogin/TeacherLogin.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import {Container, Row, Col, CardGroup, Card, CardBody, Button, Input, InputGroup, InputGroupAddon} from 'reactstrap';
 import {Link} from 'react-router-dom';
+import Toggle from 'react-toggle'
+import "react-toggle/style.css"
 import './login.css';
 
-class Login extends Component {
+class TeacherLogin extends Component {
   render() {
     return (
       <div className="app flex-row default-bg">
@@ -14,7 +16,7 @@ class Login extends Component {
                 <img className="bubbles-teacher" src={"/img/bubbles-img.png"} alt={"bubbles-teacher"}/>
               </div>
               <Link to={'/teacher-login'}>
-                <Button color="primary" className="px-4">Acessar como professor</Button>
+                <Button color="primary" className="px-4">Acessar como aluno</Button>
               </Link>
             </Col>
           </Row>
@@ -32,17 +34,25 @@ class Login extends Component {
                     <img className="bubbles-bottom" src={"/img/bubbles-img.png"} alt={"bubbles-bottom"}/>
                   </div>
                   <CardBody>
-                    <h1>Login</h1>
-                    <label> Nome </label>
+                    <h1>Login do Professor</h1>
+                    <label> Usuário </label>
                     <InputGroup className="mb-3">
                       <Input type="text"/>
                     </InputGroup>
 
-                    <label> INSERIR CÓDIGO </label>
+                    <label> Senha </label>
                     <InputGroup className="mb-4">
                       <Input type="text"/>
                     </InputGroup>
-                    <Row>
+                    <Row className="action-buttons">
+                      <Col xs="3" className="student-login-button teacher-toggle">
+                        <label>
+                          <Toggle
+                            className='custom-classname'
+                            icons={false}
+                            onChange={this.handleAubergineChange} />
+                        </label>
+                      </Col>
                       <Col xs="6" className="student-login-button">
                         <Link to={'/contato'}>
                           <Button color="primary" className="px-4">Acessar</Button>
@@ -50,14 +60,21 @@ class Login extends Component {
                       </Col>
                     </Row>
                   </CardBody>
-                </Card>
-                <Card className="d-none text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
-                  <CardBody className="text-center">
-                    <div>
-                      <h2>Cadastro</h2>
-                      <p>Para se cadastrar entre em contato com a equipe do Europa!</p>
-                    </div>
-                  </CardBody>
+                  <Row>
+                    <Col xs="12" className="teacher-register-button">
+                      <Link to={'/register'}>
+                        <Button color="primary" className="px-4">Criar conta</Button>
+                      </Link>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col xs="12" className="forgot-password-button">
+                      <Link to={'/forgot_password'}>
+                        <Button color="primary" className="px-4">Esqueceu a senha</Button>
+                      </Link>
+                    </Col>
+                  </Row>
                 </Card>
               </CardGroup>
             </Col>
@@ -68,4 +85,4 @@ class Login extends Component {
   }
 }
 
-export default Login;
+export default TeacherLogin;
