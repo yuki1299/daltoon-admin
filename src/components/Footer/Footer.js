aec57b0a9425f918f7512ef8c0ea7c36bf538138
Vinicius Lima <vinicius@loldesign.com.br>
2018-05-25 01:20:07 -0300

    Adding old daltoon project to new daltoon project Europa > MatchMapp Admin

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,6 @@ class Footer extends Component {
   render() {
     return (
       <footer className="app-footer">
-        <span><a href="http://www.loldesign.com.br">Matchmapp</a> &copy; {(new Date()).getFullYear()}</span>
-        <span className="ml-auto">Desenvolvido por <a href="http://www.loldesign.com.br">Loldesign</a></span>
       </footer>
     )
   }
